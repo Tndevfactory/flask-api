c55432956b198d984665e02b639a6316c2a15f97
Tndevfactory <tndevfactory@gmail.com>
2022-04-17 14:59:09 +0100

    class user ok

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import jwt
@@ -46,6 +47,29 @@ class ProductSchema(ma.Schema):
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
+# User Class/Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+    def __init__(self, email, password, name):
+        self.email = email
+        self.password = password
+        self.name = name
+
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'email', 'name')
+
+
+# Init Schema
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
 
 # Fpp
 @app.route('/', methods=['GET'])
@@ -60,9 +84,11 @@ def add_product():
     description = request.json['description']
     price = request.json['price']
     qty = request.json['qty']
+
     new_product = Product(name, description, price, qty)
     db.session.add(new_product)
     db.session.commit()
+
     return product_schema.jsonify(new_product)
 
 
@@ -84,15 +110,18 @@ def get_product(id):
 # update product
 @app.route('/product/<id>', methods=['PUT'])
 def update_product(id):
+    
     product = Product.query.get(id)
     name = request.json['name']
     description = request.json['description']
     price = request.json['price']
     qty = request.json['qty']
+
     product.name = name
     product.description = description
     product.price = price
     product.qty = qty
+
     db.session.commit()
     return product_schema.jsonify(product)
 
@@ -131,7 +160,7 @@ def token_required(func):
         auth_header = ( request.headers["Authorization"] if "Authorization" in request.headers else "" )
         if auth_header:
             data = request.headers['Authorization']
-            token = str.replace(str(data), 'Bearer ', '')
+            token = str.replace(str(data), 'Bearer ', '')   
         else:
             return jsonify({'message': 'Token is missing'}), 403
         try:
@@ -157,19 +186,48 @@ def protected():
     return jsonify({'message': 'only for valid token'})
 
 
+
 # login
 @app.route('/login', methods=['POST'])
 def login():
+
     dt = datetime.now() + timedelta(days=2)
 
     email = request.json['email']
     password = request.json['password']
-    if email and password == 'password':
-        token = jwt.encode({'user': email, 'exp': dt
-                            }, app.config['SECRET_KEY'], algorithm='HS256')
-        return jsonify({'user': email, 'token': token.decode('UTF-8')})
+
+    user = User.query.filter_by(email=email).first()
+
+    #return user_schema.jsonify(user)
+    if user:
+        if check_password_hash(user.password, password): #authentification
+
+            token = jwt.encode({'user': email, 'exp': dt
+                                }, app.config['SECRET_KEY'], algorithm='HS256') #authorization
+            return jsonify({'user': email, 'token': token.decode('UTF-8')})
+
     return make_response('could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    email = request.json['email']
+    name = request.json['name']
+    password = request.json['password']
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, 
+         return jsonify({'res': 'email already taken', })
+
+    # create a new user  Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'res': ' user subscribed with success ', })
 
 # run server
 if __name__ == "__main__":
